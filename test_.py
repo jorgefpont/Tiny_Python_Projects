092e854c0e0f7a57415c9e2da646505e2787f315
jorgefpont <jorgefpont@gmail.com>
2023-06-11 11:06:53 -0700

    commented changes

--- test_.py
+++ test_.py
@@ -27,6 +27,7 @@ def test_executable():
     """Says 'Hello, World!' by default"""
 
     #out = getoutput(prg)
+    # added 'python3'
     out = getoutput(f'python3 {prg}')
     assert out.strip() == 'Hello, World!'
 
@@ -36,6 +37,7 @@ def test_usage():
     """usage"""
 
     for flag in ['-h', '--help']:
+        # added 'python3'
         rv, out = getstatusoutput(f'python3 {prg} {flag}')
         assert rv == 0
         assert out.lower().startswith('usage')
@@ -47,6 +49,7 @@ def test_input():
 
     for val in ['Universe', 'Multiverse']:
         for option in ['-n', '--name']:
+            # added 'python3'
             rv, out = getstatusoutput(f'python3 {prg} {option} {val}')
             assert rv == 0
             assert out.strip() == f'Hello, {val}!'
